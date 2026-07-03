6cd34bb794183d93022139e76c48f6359d70ed6c
Salieri009 <kordalek@naver.com>
2026-07-03 09:31:11 +0900

    [Passed] 문자 출력하기 (2ms, 1MB)

---
+++ trail0/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char input;
+
+    cin>>input;
+
+    for(int i = 0; i < 8 ; i ++){
+        cout<<input;
+    }
+
+    return 0;
+}
