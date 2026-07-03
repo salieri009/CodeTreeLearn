5e1bf7ca9d85a84627e82fd2002adfc76e9dad18
Salieri009 <kordalek@naver.com>
2026-07-03 09:09:59 +0900

    [Passed] 비교 연산 (2ms, 0MB)

---
+++ trail0/비교 연산/comparison-operator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a, b;
+
+    // 입력
+    cin >> a >> b;
+
+    // a >= b
+    if (a >= b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    // a > b
+    if (a > b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    // a <= b
+    if (a <= b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    // a < b
+    if (a < b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    // a == b
+    if (a == b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    // a != b
+    if (a != b) cout << 1 << "\n";
+    else cout << 0 << "\n";
+
+    return 0;
+}
