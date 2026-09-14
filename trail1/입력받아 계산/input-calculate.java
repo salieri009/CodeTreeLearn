d147b062d49386b4d68dedc2cf10d8308911a5ed
Jungwook Van <kordalek@naver.com>
2026-09-14 17:12:27 +0900

    [Passed] 입력받아 계산 (137ms, 10MB)

---
+++ trail1/입력받아 계산/input-calculate.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        a = scanner.nextInt();
+        System.out.println(a + 2);
+    }
+}
