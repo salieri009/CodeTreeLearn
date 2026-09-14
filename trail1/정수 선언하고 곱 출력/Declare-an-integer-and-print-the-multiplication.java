a691c5e6b6d786de0c3d446afc8976674f3d79a4
Jungwook Van <kordalek@naver.com>
2026-09-14 16:57:51 +0900

    [Passed] 정수 선언하고 곱 출력 (112ms, 9MB)

---
+++ trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 26;
+        int b = 5; 
+
+
+
+        // Or, using printf for formatted output
+        System.out.printf("26 * 5 = %d%n", a * b);
+    }
+}
