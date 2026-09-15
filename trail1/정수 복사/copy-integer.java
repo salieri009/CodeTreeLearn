28cd3ee1ac7d9340af8152a1ab87a2f6e4ae39f3
Jungwook Van <kordalek@naver.com>
2026-09-15 18:44:12 +1000

    [Passed] 정수 복사 (92ms, 9MB)

---
+++ trail1/정수 복사/copy-integer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+
+        b = a;
+
+        // 줄바꿈 없이 출력
+        System.out.println(a + " " + b);
+
+        System.out.println(a * b);
+    }
+}
