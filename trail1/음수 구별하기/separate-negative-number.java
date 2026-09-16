7cec0ef367bd9c6151e4cac3d7c15ec8f397aebd
Jungwook Van <kordalek@naver.com>
2026-09-16 14:05:38 +1000

    [Passed] 음수 구별하기 (130ms, 10MB)

---
+++ trail1/음수 구별하기/separate-negative-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a;
+        Scanner sc = new Scanner(System.in);
+        
+        a =sc.nextInt();
+
+        System.out.println(a);
+        if(a<0){
+            System.out.println("minus");
+        }
+
+        
+    }
+}
