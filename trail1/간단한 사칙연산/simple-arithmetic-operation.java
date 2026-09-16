028fc72252631b36e65ced6cd137b5174cae5b9a
Jungwook Van <kordalek@naver.com>
2026-09-16 13:51:17 +1000

    [Passed] 간단한 사칙연산 (146ms, 10MB)

---
+++ trail1/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+        int b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+
+
+    }
+}
