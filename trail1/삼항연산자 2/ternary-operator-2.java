dd20f738026072d4263b68275885b6166efa1d15
Jungwook Van <kordalek@naver.com>
2026-09-22 08:28:15 +0900

    [Passed] 삼항연산자 2 (133ms, 11MB)

---
+++ trail1/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        // 삼항연산자 사용
+        String result = (a == 1) ? "t" : "f";
+
+        System.out.println(result);
+    }
+}
