2d4b909293b06408ed94644e1e54763500324901
Jungwook Van <kordalek@naver.com>
2026-09-22 20:15:42 +0900

    [Passed] 증가시키며 출력하기 3 (87ms, 9MB)

---
+++ trail1/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int a = 10;
+
+        while(a<=26){
+            System.out.print(a+" ");
+
+            a+=2;
+        }
+    }
+}
