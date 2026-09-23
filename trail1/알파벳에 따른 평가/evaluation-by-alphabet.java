2e50d485e815f1a7fbd9047c0790891b4c2f0d58
Jungwook Van <kordalek@naver.com>
2026-09-23 13:50:57 +0900

    [Passed] 알파벳에 따른 평가 (119ms, 10MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        if (a.equals("S")) {
+            System.out.println("Superior");
+        }
+        else if (a.equals("A")) {
+            System.out.println("Excellent");
+        }
+        else if (a.equals("B")) {
+            System.out.println("Good");
+        }
+        else if (a.equals("C")) {
+            System.out.println("Usually");
+        }
+        else if (a.equals("D")) {
+            System.out.println("Effort");
+        }else{
+            System.out.println("Failure");
+        }
+    }
+}
