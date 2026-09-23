90cc514bcee241d13fbf06a2a3c778ded8713da3
Jungwook Van <kordalek@naver.com>
2026-09-23 14:09:41 +0900

    [Passed] 두 번의 연산 (133ms, 10MB)

---
+++ trail1/두 번의 연산/two-operations.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+
+        if(A%2==1){
+            A+=3;
+        }
+
+        if(A%3==0){
+            A=A/3;
+        }
+
+        System.out.println(A);
+    }
+}
