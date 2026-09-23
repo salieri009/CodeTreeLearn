d3565972789003e7bdba554ecc1da05bc11b974c
Jungwook Van <kordalek@naver.com>
2026-09-23 18:43:45 +0900

    [Passed] 문자열 출력하기 (133ms, 10MB)

---
+++ trail1/문자열 출력하기/print-string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        for(;i>0;i--){
+            System.out.println("LeebrosCode");
+        }
+    }
+}
