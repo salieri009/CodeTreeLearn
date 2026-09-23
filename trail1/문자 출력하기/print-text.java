014b09a7fc88e5a814127e4f74d42bf20f6c11b7
Jungwook Van <kordalek@naver.com>
2026-09-23 17:05:33 +0900

    [Passed] 문자 출력하기 (125ms, 10MB)

---
+++ trail1/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        for(int i=1;i<9;i++){
+            System.out.print(a);
+        }
+    }
+}
